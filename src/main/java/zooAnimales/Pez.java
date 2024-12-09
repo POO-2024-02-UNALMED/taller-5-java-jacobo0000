1b37174a9f982d331c54ea5f4739770446efb07b
jacobo0000 <jacoboleal.07@gmail.com>
2024-12-09 17:31:16 -0500

    Test 5

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -14,7 +14,8 @@ public class Pez extends Animal{
     listado.add(this);
   }
 
-  public Pez(String nombre, int edad, String habitad, String genero, String colorEscamas, int cantidadAletas){
+  public Pez(String nombre, int edad, String habitat, String genero, String colorEscamas, int cantidadAletas){
+    super(nombre, edad, habitat, genero);
     this.colorEscamas = colorEscamas;
     this.cantidadAletas = cantidadAletas;
     listado.add(this);
